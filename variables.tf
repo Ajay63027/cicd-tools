9bc63bf6ad0c58e272bd39b3a1a5d70664acbda7
Ajay <ajaylearner01@gmail.com>
2025-08-19 11:46:11 +0530

    jenkins

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "ajay6.space"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z07396722PJPPRJILIZMP"
+  description = "description"
+}
